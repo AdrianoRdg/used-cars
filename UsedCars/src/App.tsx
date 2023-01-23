d8d35f49c1cff19eb57a95d229acfd8342119fd0
adriano <adrianoo.rdg@gmail.com>
2023-01-23 14:59:42 -0300

    feat: iniciando frontend do projeto

---
+++ UsedCars/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ UsedCars/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="email">
+          Email
+          <input type="text" id="email" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
